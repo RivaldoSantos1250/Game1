ca0427bd54ff5968b8b5dfa32e02d29bd5848645
RivaldoSantos1250 <rivaldosantos1250@gmail.com>
2024-04-05 17:45:00 -0300

    a

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -5,16 +5,49 @@ let prev = document.getElementById("prev");
 let next = document.getElementById("next");
 
 let active = 0;
-let lemgtItems = items.length -1;
+let lengthItems = items.length - 1;
 
 next.onclick = function () {
-    if(ative + 1 > lengthItems) {
-        
+    if (active + 1 > lengthItems) {
+        active = 0;
+    } else {
+        active = active + 1;
     }
     reloadSlider();
 };
 
+prev.onclick = function () {
+    if (active - 1 < 0) {
+        active = lengthItems;
+    } else {
+        active = active - 1;
+    }
+
+    reloadSlider();
+};
+
+let refreshSlider = setInterval(() => { next.click()
+    
+}, 3000);
+
+
 function reloadSlider() {
-    let checkLeft = items(active).offsetLeft;
+    let checkLeft = items[active].offsetLeft;
     list.style.left = -checkLeft + "px";
+
+    let lastActiveDot = document.querySelector(".slider .dots li.active");
+    lastActiveDot.classList.remove("active");
+    dots[active].classList.add("active");
+    clearInterval(refreshSlider)
+    let refreshSlider = setInterval(() => { next.click()
+    
+    }, 3000);
+    
 }
+
+dots.forEach((li, key) => {
+    li.addEventListener("click", function () {
+        active = key;
+        reloadSlider();
+    });
+});
